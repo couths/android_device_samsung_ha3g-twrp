32a6c72849214caef9f7e557dc87a569a8e3bfa7
couths <couths1@gmail.com>
2026-04-18 13:48:24 +0300

    BoardConfig.mk: adapt to 7.1
    Signed-off-by: couths <couths1@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ TARGET_BOARD_PLATFORM_GPU := mali-t628mp6
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DNO_SECURE_DISCARD
+TARGET_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DNO_SECURE_DISCARD
 
 # Architecture
 TARGET_ARCH := arm
@@ -46,6 +46,10 @@ BOARD_CUSTOM_BOOTIMG_MK :=  device/samsung/ha3g/bootimg.mk
 
 # TWRP specific build flags
 TW_THEME := portrait_hdpi
+TW_EXCLUDE_NANO := true
+TW_EXCLUDE_BASH := true
+TW_DEFAULT_LANGUAGE := en
+TWRP_INCLUDE_LOGCAT := true
 RECOVERY_SDCARD_ON_DATA := true
 BOARD_HAS_NO_REAL_SDCARD := true
 TW_BRIGHTNESS_PATH := "/sys/devices/platform/s5p-mipi-dsim.1/backlight/panel/brightness"
